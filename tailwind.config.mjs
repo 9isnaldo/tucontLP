8fdfcd9abc6d53dac62fabf258149f213256c3f6
Diego Dias Pereira <diego@iuptec.com.br>
2023-12-18 17:21:00 -0300

    Inserindo logo e mudancas das cores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,14 +4,26 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'background-primary': '#06081E',
-				'background-secondary': '#0D1439',
-				'primary-color': '#1f4287',
-				'secondary-color': '#3D5AF1',
-				'tertiary-color': '#278ea5',
-				'quaternary-color': '#22D1EE',
-				'quintenary-color': '#21E6C1',
-				'sextiary-color': '#e2f2f4'
+				'rich-black': '#06081E',
+				'oxford-blue': '#0D1439',
+				'primary': {
+					DEFAULT: '#1f4287'
+				},
+				'secondary': {
+					DEFAULT: '#3D5AF1'
+				},
+				'tertiary': {
+					DEFAULT: '#278ea5'
+				},
+				'quaternary': {
+					DEFAULT: '#22D1EE'
+				},
+				'quintenary': {
+					DEFAULT: '#21E6C1'
+				},
+				'sextiary': {
+					DEFAULT: '#e2f2f4'
+				}
 			},
 		},
 	},
