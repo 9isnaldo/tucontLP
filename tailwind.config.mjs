c8eae6f7b86f769f8205e743cc61f5baa394c71b
victor <victor@iuptec.com.br>
2023-12-18 17:02:58 -0300

    feat: adicionando paleta

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'background-primary': '#06081E',
+				'background-secondary': '#0D1439',
+				'primary-color': '#1f4287',
+				'secondary-color': '#3D5AF1',
+				'tertiary-color': '#278ea5',
+				'quaternary-color': '#22D1EE',
+				'quintenary-color': '#21E6C1',
+				'sextiary-color': '#e2f2f4'
+			},
+		},
 	},
 	plugins: [],
 }
