06b6060dedecafd06a987d787a8fa38d1a00c541
Diego Dias Pereira <diego@iuptec.com.br>
2023-12-20 10:47:11 -0300

    fix: change to serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true
